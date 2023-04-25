03e1063bf06bdb664fb05f7c1973900d7768b34f
DmitryMarhalik <dmitrymarhalik.gmail.com>
2023-04-25 18:26:02 +0300

    Home Work lesson8 - OOP_authen_registr_version

---
+++ lesson_8/OOP_authen_registr.py
@@ -0,0 +1,77 @@
+import json
+
+with open("data_base_authentication.json", "r") as my_json_file:
+    data_base = json.load(my_json_file)
+
+
+class User:
+    """Введенные данные пользователя"""
+
+    def __init__(self, input_name, input_password):
+        self.name = input_name
+        self.password = input_password
+
+
+try:
+    class VerifyNamePassword(User):
+        """Проверка введенных данных пользователя"""
+
+        def __init__(self):
+            super().__init__(user.name, user.password)
+
+        def check_name(self):
+            if len(user.name) < 2:
+                print("Error.The name is too short."
+                      "The name must contain more than 1 character")
+            elif len(user.name) > 15:
+                print("Error.The name is too long."
+                      "The name must contain less than 15 character")
+            else:
+                return user.name
+
+        def check_password(self):
+            if user.name not in data_base:
+                print("Authentication Error. Check name")
+            elif user.password == data_base[user.name]:
+                print(f"Hey,{user.name}!")
+                return True
+            else:
+                print("Authentication Error. Check password")
+
+
+    class Registration(User):
+        """Регистрация пользователя. Довавление имени и пароля пользователя в базу данных"""
+
+        def __init__(self):
+            super().__init__(user.name, user.password)
+
+        def registration_name_and_passwords(self):
+            if user.name in data_base:
+                print("Your name has already been registered")
+            else:
+                data_base.update({user.name: user.password})
+                print("You have been successfully registered")
+                with open("data_base_authentication.json", "w") as json_file:
+                    json.dump(data_base, json_file, indent=4)
+            return data_base
+
+
+    while True:
+        choice = input("For 'authentication' enter 'a' and press 'Enter',\n"
+                       "For 'registration' enter 'r' and press 'Enter: \n")
+        if choice == "a":
+            user = User(input("Enter name: "), input("Enter password: "))
+            varify_name = VerifyNamePassword.check_name(user.name)
+            if varify_name:
+                varify_password = VerifyNamePassword.check_password(user.password)
+                if varify_password:
+                    break
+        elif choice == "r":
+            user = User(input("Enter name: "), input("Enter password: "))
+            varify_name = VerifyNamePassword.check_name(user.name)
+            if varify_name:
+                Registration.registration_name_and_passwords(user.password)
+        else:
+            print("Please, select 'a' or 'r'")
+except KeyboardInterrupt:
+    print("\n""You have STOPPED the execution of the program!")
---
+++ lesson_8/OOP_authen_registr_second_version.py
@@ -0,0 +1,70 @@
+import json
+
+with open("data_base_authentication.json", "r") as my_json_file:
+    data_base = json.load(my_json_file)
+
+
+class User:
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+
+try:
+    class VerifyNamePassword(User):
+        def __init__(self):
+            User.__init__(self, self.name, self.password)
+
+        def check_name(self):
+            if len(us.name) < 2:
+                print("Error.The name is too short."
+                      "The name must contain more than 1 character")
+            elif len(us.name) > 15:
+                print("Error.The name is too long."
+                      "The name must contain less than 15 character")
+            else:
+                return us.name
+
+        def check_password(self):
+            if us.name not in data_base:
+                print("Authentication Error. Check name")
+            elif us.password == data_base[us.name]:
+                print(f"Hey,{us.name}!")
+                return True
+            else:
+                print("Authentication Error. Check password")
+
+
+    class Registration(User):
+        def __init__(self):
+            User.__init__(self, self.name, self.password)
+
+        def registration_name_and_passwords(self):
+            if us.name in data_base:
+                print("Your name has already been registered")
+            else:
+                data_base.update({us.name: us.password})
+                print("You have been successfully registered")
+                with open("data_base_authentication.json", "a") as my_json_file:
+                    json.dump(data_base, my_json_file, indent=4)
+            return data_base
+
+
+    while True:
+        choice = input("For 'authentication' enter 'a' and press 'Enter',\n"
+                       "For 'registration' enter 'r' and press 'Enter: \n")
+        if choice == "a":
+            us = User(input("Enter name: "), input("Enter password: "))
+            varify_name = VerifyNamePassword.check_name(us.name)
+            if varify_name:
+                varify_password = VerifyNamePassword.check_password(us.password)
+                if varify_password:
+                    break
+        elif choice == "r":
+            us = User(input("Enter name: "), input("Enter password: "))
+            varify_name = VerifyNamePassword.check_name(us.name)
+            if varify_name:
+                Registration.registration_name_and_passwords(us.password)
+        else:
+            print("Please, select 'a' or 'r'")
+except KeyboardInterrupt:
+    print("\n""You have STOPPED the execution of the program")
