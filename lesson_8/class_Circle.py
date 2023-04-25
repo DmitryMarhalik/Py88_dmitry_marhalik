3c4372e99743dabf1df5290dcb26cf1e45edbe19
DmitryMarhalik <dmitrymarhalik.gmail.com>
2023-04-25 18:28:48 +0300

    Вычитание двух окружностей

---
+++ lesson_8/class_Circle.py
@@ -0,0 +1,49 @@
+import math
+
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, second_circle):
+        return self.x == second_circle.x and self.y == second_circle.y
+
+    def __add__(self, second_circle):
+        x = self.x + second_circle.x
+        y = self.y + second_circle.y
+        return Point(x, y)
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+    def distance_from_origin(self):
+        return math.hypot(self.x, self.y)
+
+
+class Circle(Point):
+
+    def __init__(self, radius, x=0, y=0):
+        super().__init__(x, y)
+        self.radius = radius
+
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+    def __str__(self):
+        return super().__str__()[:-1] + f', radius={self.radius})'
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        radius = self.radius + other.radius
+        return Circle(radius, x, y)
+
+    def edge_distance_from_origin(self):
+        return abs(self.distance_from_origin() - self.radius)
+
+    def circumference(self):
+        return 2 * math.pi * self.radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
