5a6ba5f5be89341e597833ccde5d95919690b361
DmitryMarhalik <dmitrymarhalik.gmail.com>
2023-05-08 16:10:30 +0300

    add 3 files lesson 10 homework

---
+++ lesson_10/authentication_OOP_exception_modules.py
@@ -0,0 +1,60 @@
+import json
+
+from authentication_and_data_storage import AuthenticationSystem, data_base
+from user import User
+
+
+class StartProgram:
+
+    def get_login(self):
+        return input("Enter login: ")
+
+    def get_password(self):
+        return input("Enter password: ")
+
+    def get_age(self):
+        return input("Enter age: ")
+
+    def start(self):
+        while True:
+            choosing_an_action = input("\nfor 'authentication' enter 'a' and press 'Enter'\n"
+                                       "for 'registration' enter 'r' and press 'Enter': \n")
+            if choosing_an_action == "a":
+                login, password = StartProgram().get_login(), StartProgram().get_password()
+                if AuthenticationSystem().check_len_login(login) and \
+                        AuthenticationSystem().check_login_password(login, password):
+                    break
+                elif not AuthenticationSystem().check_login_in_data_base(login) or \
+                        not AuthenticationSystem().check_len_login(login):
+                    raise NameError
+
+            elif choosing_an_action == "r":
+                login, password, age = StartProgram().get_login(), StartProgram().get_password(), StartProgram().get_age()
+                if AuthenticationSystem().check_len_login(login):
+                    RegistrationSystem().registration_name_and_passwords(login, password, age)
+                else:
+                    raise NameError
+            else:
+                print("Please, select 'a' or 'r'")
+
+
+class RegistrationSystem(AuthenticationSystem):
+    """Регистрация пользователя. Довавление имени и пароля пользователя в базу данных"""
+
+    def registration_name_and_passwords(self, login, password, age):
+        if AuthenticationSystem.check_login_in_data_base(self, login):
+            print("Your login has already been registered")
+        else:
+            client = User(login, password, age)
+            data_base.append({
+                "login": client.login,
+                "password": client.password,
+                "age": client.age
+            })
+            print("You have been successfully registered")
+            with open("data_base_authentication.json", "w") as json_file:
+                json.dump(data_base, json_file, indent=4)
+        return data_base
+
+
+StartProgram().start()
