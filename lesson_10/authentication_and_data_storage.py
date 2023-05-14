4376b8e8cf744d6817eb0da2f22413ade4d13836
DmitryMarhalik <dmitrymarhalik.gmail.com>
2023-05-15 00:46:39 +0300

    lesson 10 HM

---
+++ lesson_10/authentication_and_data_storage.py
@@ -0,0 +1,53 @@
+import json
+
+
+class DataStorage:
+    PATH_TO_STORE = "/home/dm/PycharmProjects/pythonProject/dmhome/lesson_10/data_base_authentication.json"
+
+    def open_data(self):
+        try:
+            with open(DataStorage.PATH_TO_STORE, "r") as my_json_file:
+                data_base = json.load(my_json_file)
+        except FileNotFoundError:
+            print("data_base file not found, please complete the registration")
+            data_base = []
+            data_base.append({"login": "aa", "password": "0", "age": "0"})
+            with open(DataStorage.PATH_TO_STORE, "w") as json_file:
+                json.dump(data_base, json_file, indent=4)
+        return data_base
+
+
+data_base = DataStorage().open_data()
+
+
+class AuthenticationSystem:
+    """Проверка введенных данных пользователя"""
+
+    @staticmethod
+    def check_login_in_data_base(login):
+        for user in data_base:
+            if user["login"] == login:
+                return True
+
+    @staticmethod
+    def check_len_login(login):
+        if 2 > len(login) or len(login) > 25:
+            return False
+        else:
+            return login
+
+    @staticmethod
+    def chek_password(login, password):
+        for user in data_base:
+            if user["login"] == login and user["password"] == password in user.values():
+                return True
+
+    @staticmethod
+    def check_login_password(login, password):
+        if not AuthenticationSystem.check_login_in_data_base(login):
+            return False
+        elif AuthenticationSystem.chek_password(login, password):
+            print(f"Hey,{login}!")
+            return True
+        else:
+            return "Authentication Error. Check password"
