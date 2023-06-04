743ddfd5479efe564d0c15e0d63f42910ff44a1e
DmitryMarhalik <dmitrymarhalik.gmail.com>
2023-06-05 00:58:47 +0300

    some update tgbot

--- lesson_15/telegram_bot_Fullversion.py
+++ lesson_15/telegram_bot_Fullversion.py
@@ -17,7 +17,7 @@ bot = telebot.TeleBot(os.getenv("TOKEN"))
 def start(message):
     if check_id(message):
         markup = types.ReplyKeyboardMarkup()
-        button_1 = types.KeyboardButton("View all id of product in the database")
+        button_1 = types.KeyboardButton("View all id's of products in the database")
         button_2 = types.KeyboardButton("Add a product to the database")
         button_3 = types.KeyboardButton("Enter intake")
         button_4 = types.KeyboardButton("Calculation result")
@@ -66,7 +66,7 @@ def on_click(message):
     elif message.text == "Calculation result":
         bot.send_message(message.from_user.id, "For what time period to calculate the result? "
                                                "Enter the number of days: ")
-        bot.register_next_step_handler(message, calculation_result)
+        bot.register_next_step_handler(message, finally_calculation)
 
 
 def check_id(message):
@@ -80,67 +80,76 @@ def check_id(message):
 
 
 def add_product(message):
-    product_name, protein, fat, carbohydrate, kcal = message.text.split(" ")
-    postgres_insert_query = """INSERT INTO products (name,proteins,fats,carbohydrates,kcal)
-        VALUES (%s,%s,%s,%s,%s)"""
-    cursor.execute(postgres_insert_query, (product_name, float(protein),
-                                           float(fat), float(carbohydrate), float(kcal)))
-    connection.commit()
-    bot.send_message(message.from_user.id, "Product added successfully!")
-    connection.close()
+    try:
+        product_name, protein, fat, carbohydrate, kcal = message.text.split(" ")
+        postgres_insert_query = """INSERT INTO products (name,proteins,fats,carbohydrates,kcal)
+            VALUES (%s,%s,%s,%s,%s)"""
+        cursor.execute(postgres_insert_query, (product_name, float(protein),
+                                               float(fat), float(carbohydrate), float(kcal)))
+        connection.commit()
+        bot.send_message(message.from_user.id, "Product added successfully!")
+    except ValueError:
+        bot.send_message(message.from_user.id, "Incorrect input product. Please, try again")
 
 
 def intake(message):
-    product_id, gram = message.text.split(" ")
-    postgres_insert_query1 = """select users.id from users where users.chat_id_telegram = %s"""
-    # or postgres_insert_query1 = f"""select users.id from users where users.chat_id_telegram =
-    # {(str(message.from_user.id))}"""
-    postgres_insert_query2 = """INSERT INTO intake (user_id, product_id, gram) VALUES (%s,%s,%s)"""
+    try:
+        product_id, gram = message.text.split(" ")
+        postgres_insert_query1 = """select users.id from users where users.chat_id_telegram = %s"""
+        # or postgres_insert_query1 = f"""select users.id from users where users.chat_id_telegram =
+        # {(str(message.from_user.id))}"""
+        postgres_insert_query2 = """INSERT INTO intake (user_id, product_id, gram) VALUES (%s,%s,%s)"""
+        cursor.execute(postgres_insert_query1, (message.from_user.id,))
+        user_id = cursor.fetchone()[0]
+        cursor.execute(postgres_insert_query2, (user_id, int(product_id), float(gram)))
+        connection.commit()
+        bot.send_message(message.from_user.id, "The intake added!")
+    except ValueError:
+        bot.send_message(message.from_user.id, "Incorrect input intake. Please, try again")
 
-    cursor.execute(postgres_insert_query1, (message.from_user.id,))
-    user_id = cursor.fetchone()[0]
-    cursor.execute(postgres_insert_query2, (user_id, int(product_id), float(gram)))
-    connection.commit()
-    bot.send_message(message.from_user.id, "The intake added!")
-    connection.close()
+
+def calculation_all_idproducts(message):
+    try:
+        days = int(message.text)
+        postgres_insert_query = f"""select product_id from intake where (time > now() - interval '%s days' 
+          and user_id = {str(check_id(message))})"""
+        cursor.execute(postgres_insert_query, (days,))
+        return cursor.fetchall()
+    except ValueError:
+        bot.send_message(message.from_user.id, "Incorrect input days. Please, try again")
+        return False
 
 
-def calculation_result(message):
-    days = int(message.text)
-    postgres_insert_query = f"""select product_id from intake where (time > now() - interval '%s days' 
-    and user_id = {str(check_id(message))})"""
-    cursor.execute(postgres_insert_query, (days,))
-    idproducts = cursor.fetchall()
+def finally_calculation(message):
+    idproducts = calculation_all_idproducts(message)
+    if idproducts != False:
+        all_products = view_all_products = all_energy_values = nice_count_of_product = []
+        for id in idproducts:
+            postgres_insert_query = """select name from products where id=%s"""
+            cursor.execute(postgres_insert_query, id)
+            product = cursor.fetchone()
+            all_products.append(product)
 
-    all_products = []
-    for id in idproducts:
-        postgres_insert_query = """select name from products where id=%s"""
-        cursor.execute(postgres_insert_query, id)
-        product = cursor.fetchone()
-        all_products.append(product)
+        for name in all_products:
+            view_all_products.append(name[0])
+            postgres_insert_query = """select proteins,fats,carbohydrates, kcal from products where name = %s"""
+            cursor.execute(postgres_insert_query, name)
+            energy_values = cursor.fetchone()
+            all_energy_values.append(energy_values)
 
-    view_all_products, all_energy_values = [], []
-    for name in all_products:
-        view_all_products.append(name[0])
-        postgres_insert_query = """select proteins,fats,carbohydrates, kcal from products where name = %s"""
-        cursor.execute(postgres_insert_query, name)
-        energy_values = cursor.fetchone()
-        all_energy_values.append(energy_values)
-
-    all_proteins = all_fats = all_carbohydrates = all_kcal = 0
-    for el in all_energy_values:
-        all_proteins += el[0]
-        all_fats += el[1]
-        all_carbohydrates += el[2]
-        all_kcal += el[3]
-    count_of_product = Counter(view_all_products)
-    nice_count_of_product = []
-    for k, v in count_of_product.items():
-        nice_count_of_product.append(f"{k}->{v} times\n")
-    result_message = f"Нou have eaten the following foods:\n{''.join(nice_count_of_product)}\n" \
-                     f"Total amount of protein:  {all_proteins} gr,\nfats: {all_fats} gr,\n" \
-                     f"carbohydrates: {all_carbohydrates} gr,\nKcal: {all_kcal}"
-    bot.send_message(message.from_user.id, result_message)
+        all_proteins = all_fats = all_carbohydrates = all_kcal = 0
+        for el in all_energy_values:
+            all_proteins += el[0]
+            all_fats += el[1]
+            all_carbohydrates += el[2]
+            all_kcal += el[3]
+        count_of_product = Counter(view_all_products)
+        for k, v in count_of_product.items():
+            nice_count_of_product.append(f"{k}->{v} times\n")
+        result_message = f"Нou have eaten the following foods:\n{''.join(nice_count_of_product)}\n" \
+                         f"Total amount of protein:  {all_proteins} gr,\nfats: {all_fats} gr,\n" \
+                         f"carbohydrates: {all_carbohydrates} gr,\nKcal: {all_kcal}"
+        bot.send_message(message.from_user.id, result_message)
 
 
 bot.polling(none_stop=True)
